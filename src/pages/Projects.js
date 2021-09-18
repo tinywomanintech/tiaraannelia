608294c8e5f828dc08ad95c68e898b6b6fbb5abb
Tiara <56816858+tiarannelia@users.noreply.github.com>
2021-09-18 16:42:19 +0700

    added projects pages

---
+++ src/pages/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Projectsme from '../components/Projectsme';
+import Footer from '../parts/Footer';
+import Header from '../parts/Header';
+
+const Projects = () => {
+  return (
+    <div>
+      <Header />
+      <Projectsme />
+      <Footer />
+    </div>
+  );
+};
+
+export default Projects;
