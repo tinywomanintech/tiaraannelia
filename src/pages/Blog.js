32de570abb5adc9aae68c8a80abb960d86409ec4
Tiara <56816858+tiarannelia@users.noreply.github.com>
2021-09-18 16:41:29 +0700

    added blog pages

---
+++ src/pages/Blog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Blogs from '../components/Blog';
+import Footer from '../parts/Footer';
+import Header from '../parts/Header';
+
+const Blog = () => {
+  return (
+    <div>
+      <Header />
+      <Blogs />
+      <Footer />
+    </div>
+  );
+};
+
+export default Blog;
