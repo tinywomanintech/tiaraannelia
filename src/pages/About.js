0907d41b9eb38a2e0b3770ed600a70b1a29aee22
Tiara <56816858+tiarannelia@users.noreply.github.com>
2021-09-18 16:41:00 +0700

    added about pages

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aboutme from '../components/Aboutme';
+import Footer from '../parts/Footer';
+import Header from '../parts/Header';
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <Aboutme />
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
