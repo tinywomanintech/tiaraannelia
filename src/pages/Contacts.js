f279317bec8c91cc5928a08eb839ff55b75ef40a
Tiara <56816858+tiarannelia@users.noreply.github.com>
2021-09-18 16:41:54 +0700

    added contact pages

---
+++ src/pages/Contacts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Contact from '../components/Contact';
+import Footer from '../parts/Footer';
+import Header from '../parts/Header';
+
+const Contacts = () => {
+  return (
+    <div>
+      <Header />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default Contacts;
